cf6c608f44f6bcf463b5e96e322907103230ef13
Steve Hudek <sjhudek@gmail.com>
2023-08-10 12:43:33 -0600

    added tables to schema.sql

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -6,3 +6,34 @@ CREATE DATABASE ecommerce_db;
 
 -- USE DATABASE
 USE ecommerce_db;
+
+-- Create Category Table
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL
+);
+
+-- Create Product Table
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+-- Create Tag Table
+CREATE TABLE tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255)
+);
+
+-- Create ProductTag Table
+CREATE TABLE product_tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
