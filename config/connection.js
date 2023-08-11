625ab33d3d70647fe86ff4804787121b0f3b84ea
Steve Hudek <sjhudek@gmail.com>
2023-08-11 10:34:24 -0600

    updated code

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,15 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
+    process.env.DB_DATABASE, // Updated this
+    process.env.DB_USERNAME, // And this
     process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST, // Don't forget the host as well
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
 
 module.exports = sequelize;
