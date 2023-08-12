6ef700069c0872544edc148446876cecd738feb4
Steve Hudek <sjhudek@gmail.com>
2023-08-12 13:13:16 -0600

    modified routes for routes/api/category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,26 +68,26 @@ router.put('/:id', async(req, res) => {
   }
 });
 
-router.delete('/:id', async(req, res) => {
-  console.log("Inside DELETE all categories route");
+// router.put('/:id', async(req, res) => {
+//   console.log('Starting PUT operation');
+//   try {
+//     console.log('Updating category in database');
+//       const [updatedRows] = await Category.update(req.body, {
+//           where: { id: req.params.id }
+//       });
 
-  // delete a category by its `id` value
-  try {
-    const categoryData = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
+//       if (updatedRows === 0) {
+//           return res.status(404).json({ message: 'No category found with this id!' });
+//       }
+//       console.log('Finished PUT operation');
 
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//       res.status(200).json({ message: 'Category updated successfully.' });
+//   } catch (err) {
+//       res.status(400).json(err);
+//   }
+// });
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: 'Test response' });
 });
 
 module.exports = router;
