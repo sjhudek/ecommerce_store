6bc6dcb1c7d56ece5111f909209e9bf5c0f6de7c
Steve Hudek <sjhudek@gmail.com>
2023-08-11 22:13:17 -0600

    updated routes for routes/api/product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get('/api/product  ', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
+router.get('/api/product/:id', async(req, res) => {
   try {
       const productData = await Product.findByPk(req.params.id, {
           include: [{ model: Category }, { model: Tag }],
@@ -35,7 +35,7 @@ router.get('/:id', async(req, res) => {
 
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/api/product', async (req, res) => {
   try {
     const product = await Product.create(req.body);
 
@@ -56,29 +56,8 @@ router.post('/', async (req, res) => {
   }
 });
 
-  // Product.create(req.body)
-  //   .then((product) => {
-  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-  //     if (req.body.tagIds.length) {
-  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
-  //         return {
-  //           product_id: product.id,
-  //           tag_id,
-  //         };
-  //       });
-  //       return ProductTag.bulkCreate(productTagIdArr);
-  //     }
-  //     // if no product tags, just respond
-  //     res.status(200).json(product);
-  //   })
-  //   .then((productTagIds) => res.status(200).json(productTagIds))
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(400).json(err);
-  //   });
-
 // update product
-router.put('/:id', (req, res) => {
+router.put('/api/product/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -122,7 +101,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/api/product/:id', async (req, res) => {
   try {
       const productData = await Product.destroy({
           where: {
