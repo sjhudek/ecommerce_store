39e982984c8174e1aa2bc3fbcf848dbe1fd8ad6c
Steve Hudek <sjhudek@gmail.com>
2023-08-11 21:04:18 -0600

    updated code for routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,9 +7,9 @@ router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
-// Default route message for the root
-router.get('/', (req, res) => {
-    res.send('Welcome to the Ecommerce API. Use /api/products, /api/tags, etc.');
+// Catch-all route for undefined routes (make sure this is LAST)
+router.use((req, res) => {
+    res.status(404).send("Wrong route!");
   });
 
 module.exports = router;
