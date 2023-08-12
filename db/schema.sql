0c389726aa83a8500f3205d12c5ca752225d4a97
Steve Hudek <sjhudek@gmail.com>
2023-08-11 22:08:55 -0600

    updated code for db/schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,32 +8,32 @@ CREATE DATABASE ecommerce_db;
 USE ecommerce_db;
 
 -- Create Category Table
-CREATE TABLE categories (
+CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
   category_name VARCHAR(255) NOT NULL
 );
 
 -- Create Product Table
-CREATE TABLE products (
+CREATE TABLE product (
   id INT AUTO_INCREMENT PRIMARY KEY,
   product_name VARCHAR(255) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
   stock INT NOT NULL DEFAULT 10,
   category_id INT,
-  FOREIGN KEY (category_id) REFERENCES categories(id)
+  FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 -- Create Tag Table
-CREATE TABLE tags (
+CREATE TABLE tag (
   id INT AUTO_INCREMENT PRIMARY KEY,
   tag_name VARCHAR(255)
 );
 
 -- Create ProductTag Table
-CREATE TABLE product_tags (
+CREATE TABLE product_tag (
   id INT AUTO_INCREMENT PRIMARY KEY,
   product_id INT,
   tag_id INT,
-  FOREIGN KEY (product_id) REFERENCES products(id),
-  FOREIGN KEY (tag_id) REFERENCES tags(id)
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
