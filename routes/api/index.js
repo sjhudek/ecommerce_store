5fbbf6b08944083d7c01dd9d323dcf4beb694726
Steve Hudek <sjhudek@gmail.com>
2023-08-11 22:12:38 -0600

    updated code for routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -10,6 +10,6 @@ router.use('/tags', tagRoutes);
 // Catch-all route for undefined routes (make sure this is LAST)
 router.use((req, res) => {
     res.status(404).send("Wrong route!");
-  });
+  });   
 
 module.exports = router;
