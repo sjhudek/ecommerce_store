f1af6b6d83beb1bf3d011222d84f6289dd9f8e5d
Steve Hudek <sjhudek@gmail.com>
2023-08-11 23:09:08 -0600

    modified code in routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,9 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use('/category', categoryRoutes);
+router.use('/product', productRoutes);
+router.use('/tag', tagRoutes);
 
 // Catch-all route for undefined routes (make sure this is LAST)
 router.use((req, res) => {
