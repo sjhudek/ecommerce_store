acdce32c720559bced514f79d251dbcc5637dfa1
Steve Hudek <sjhudek@gmail.com>
2023-08-12 22:24:31 -0600

    updated code in routes/api/product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,22 +105,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-      const productData = await Product.destroy({
-          where: {
-              id: req.params.id
-          }
-      });
+    const deletedProduct = await Product.destroy({
+      where: { id: req.params.id }
+    });
 
-      if (!productData) {
-          res.status(404).json({ message: 'No product found with this id!' });
-          return;
-      }
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'No product found with this ID.' });
+    }
 
-      res.status(200).json({ message: 'Product deleted successfully!' });
+    res.status(200).json({ message: 'Product deleted successfully.' });
   } catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
