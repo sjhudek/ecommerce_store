cd57b797c77f3e0db39561548f53562104f62792
Steve Hudek <sjhudek@gmail.com>
2023-08-13 16:00:16 -0600

    modified code in routes/api/tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, include: [Tag]}],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -17,12 +17,17 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{
+        model: Product,
+        include: [Tag]
+      }]
     });
+
     if (!tagData) {
-      res.status(404).json({ message: "No tag found with this id!" });
+      res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
